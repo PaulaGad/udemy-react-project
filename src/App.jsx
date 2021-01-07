d78d145879a055a50c5a22a1c0fb8458b4a360ce
Paula Gadomska <paulagadomska@wp.pl>
2021-01-07 17:05:27 +0100

    feat: Navigation of App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { HashRouter as Router } from 'react-router-dom'
 import './App.scss';
 import Header from './components/Header/Header';
 import StoreProvider from './store/StoreProvider'
 
 import './App.scss';
+import AsideMenu from './components/AsideMenu/AsideMenu';
 
 const App = () => {
  return (
   <StoreProvider>
    <Header/>
+   <Router>
+    <div className="content-wrapper">
+     <AsideMenu/>
+    </div>
+   </Router>
   </StoreProvider>
  );
 }
---
+++ src/components/AsideMenu/AsideMenu.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import bemCssModules from 'bem-css-modules';
+import AdminMenu from './subcomponents/AdminMenu';
+import UserMenu from './subcomponents/UserMenu';
+import { StoreContext } from '../../store/StoreProvider';
+import { default as AsideMenuStyles} from './AsideMenu.module.scss';
+
+
+
+const style = bemCssModules(AsideMenuStyles);
+
+const AsideMenu = () => {
+ const { user } = useContext(StoreContext);
+
+ const ADMIN_TYPE = 1;
+
+ const adminMenuComponent = user?.accessLevel === ADMIN_TYPE ? <AdminMenu /> : null; //optional chaining aby z tego skorzystać zainstalowaliśmy @babel/plugin-proposal-optional-chaining i dodaliśmy to do .babelrc jako plugins ({  "presets": ["@babel/preset-react"],  "plugins": ["@babel/plugin-proposal-optional-chaining"] })
+
+ return (
+  <section className={style()}>
+   <UserMenu isUserLogged={Boolean(user)}/>
+  </section>
+ );
+}
+ 
+export default AsideMenu;
